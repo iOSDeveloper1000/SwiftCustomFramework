648b5656d5d9341c0a141775a86c556ad8a98efc
Arno Seidel <aseidel-it@posteo.de>
2021-12-08 16:05:31 +0100

    chore: Initial setup of Xcode framework

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+
 # SwiftCustomFramework
-Custom Swift framework with non-specific classes, extensions and functions to use in apps of any purpose.
+
+This is a quite customized framework for use with general-purpose apps written in Swift. It contains non-specific classes, extensions and functions for utility and ease of developing new iOS apps.
+
+
+## How to use it as a pod
+
+_@TODO_
---
+++ SwiftCustomFramework/SwiftCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftCustomFramework.h
+//  SwiftCustomFramework
+//
+//  Created by Arno Seidel on 08.12.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftCustomFramework.
+FOUNDATION_EXPORT double SwiftCustomFrameworkVersionNumber;
+
+//! Project version string for SwiftCustomFramework.
+FOUNDATION_EXPORT const unsigned char SwiftCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCustomFramework/PublicHeader.h>
+
+
